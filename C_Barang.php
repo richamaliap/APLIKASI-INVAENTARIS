3a1a258284ebdd9a250d0993145e81e3f57f3faa
richamaliap <34180639+richamaliap@users.noreply.github.com>
2017-12-02 13:48:23 +0700

    hapusAdmin
    hapus show admin

--- C_Barang.php
+++ C_Barang.php
@@ -71,27 +71,5 @@ class C_Barang extends CI_Controller {
         $this->load->view('Foot/V_Footer');
     }
 
-    public function ShowByLocAdmin() {
-        $this->load->view('Head/V_Header');
-        $this->load->view('Head/V_MenuAdmin');
-        $this->load->view('Head/V_ToggleLoc');
-        $result['query'] = $this->M_Barang->ShowBarang();
-        if (filter_input(INPUT_POST, "submit")) {
-            $Lokasi = filter_input(INPUT_POST, "Lokasi");
-            if ($Lokasi == 'Gudang 1') {
-                $result['query'] = $this->M_Barang->ShowLokasi('Gudang 1');
-            } else if ($Lokasi == 'Gudang 2') {
-                $result['query'] = $this->M_Barang->ShowLokasi('Gudang 2');
-            } else if ($Lokasi == 'Gudang 3') {
-                $result['query'] = $this->M_Barang->ShowLokasi('Gudang 3');
-            } else if ($Lokasi == 'Gudang 4') {
-                $result['query'] = $this->M_Barang->ShowLokasi('Gudang 4');
-            } else {
-                $result['query'] = $this->M_Barang->ShowLokasi('Gudang 5');
-            }
-        }
-        $this->load->view('V_Location', $result);
-        $this->load->view('Foot/V_Footer');
-    }
-
+  
 }
